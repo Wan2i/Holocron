a6a7f81e13c9c2ee6e95b0aa6973305fceb8703d
Knave <safwanrodzi@gmail.com>
2026-09-04 23:35:50 +0800

    fix: format code

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -2,17 +2,17 @@ import { invoke } from "@tauri-apps/api/core";
 import type { Notes } from "../types/notes";
 
 export function getNotes(): Promise<Notes[]> {
-  return invoke("get_notes");
+    return invoke("get_notes");
 }
 
-export function createNotes(sId:number, chapter: number, name: string, filePath: string) : Promise<Notes> {
-    return invoke("create_notes", {sId, chapter, name, filePath});
+export function createNotes(sId: number, chapter: number, name: string, filePath: string): Promise<Notes> {
+    return invoke("create_notes", { sId, chapter, name, filePath });
 }
 
-export function updateNotes(nId: number, sId:number, chapter: number, name: string, filePath: string ) : Promise<Notes> {
-    return invoke("update_notes", {nId, sId, chapter, name, filePath} );
+export function updateNotes(nId: number, sId: number, chapter: number, name: string, filePath: string): Promise<Notes> {
+    return invoke("update_notes", { nId, sId, chapter, name, filePath });
 }
 
-export function deleteNotes(nId: number) : Promise<void>{
-    return invoke("delete_notes", {nId});
+export function deleteNotes(nId: number): Promise<void> {
+    return invoke("delete_notes", { nId });
 }
